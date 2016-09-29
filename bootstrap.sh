d1abdf1afe1591fe088d5a173d966265052526f4
bdevore17 <bdevore17@gmail.com>
2016-09-29 15:55:31 -0400

    xv6 compatibility

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
 sudo apt-get update
 sudo apt-get install -y valgrind
 sudo apt-get install -y git
+sudo apt-get install -y qemu-system-x86
+sudo apt-get install -y tmux
+sudo apt-get install -y gdb
+
 sudo apt-get upgrade -y
